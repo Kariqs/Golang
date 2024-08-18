7d0ebbff85d1aad69b3f3e8adbbdb7567ea5899f
unknown <kariukibenard189@gmail.com>
2024-08-18 16:42:31 +0300

    Learnt about control structures,reading from and writing to files,error handling

--- app.go
+++ app.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
+	"os"
 )
 
 func main() {
@@ -29,22 +31,37 @@ func calculate() {
 }
 
 func profitCalculator() {
-	revenue := userInput("Revenue: ")
-	expenses := userInput("Expenses: ")
-	taxRates := userInput("Tax Rates: ")
+	revenue, revenueerr := userInput("Revenue: ")
+	if revenueerr != nil {
+		fmt.Println(revenueerr)
+		return
+	}
+	expenses, expenseerr := userInput("Expenses: ")
+	if expenseerr != nil {
+		fmt.Println(expenseerr)
+		return
+	}
+	taxRates, rateerr := userInput("Tax Rates: ")
+	if rateerr != nil {
+		fmt.Println(rateerr)
+		return
+	}
 
 	var earningsBeforeTax, earningsAfterTax, ratio = earnings(revenue, expenses, taxRates)
-
+	writeResultsToFile(earningsBeforeTax, earningsAfterTax, ratio)
 	fmt.Println("Earnings Before Tax (EBT):", earningsBeforeTax)
 	fmt.Println("Earnings After Tax (EAT):", earningsAfterTax)
 	fmt.Printf("Ratio of EBT to EAT:%.2f", ratio)
 }
 
-func userInput(text string) float64 {
+func userInput(text string) (float64, error) {
 	var inputText float64
 	fmt.Print(text)
 	fmt.Scan(&inputText)
-	return inputText
+	if inputText <= 0 {
+		return 0, errors.New("invalid value, value cannot be 0 or less than 0")
+	}
+	return inputText, nil
 }
 
 func earnings(revenue, expenses, taxRates float64) (float64, float64, float64) {
@@ -52,4 +69,9 @@ func earnings(revenue, expenses, taxRates float64) (float64, float64, float64) {
 	var eat float64 = ebt * (1 - (taxRates / 100))
 	var ratio float64 = ebt / eat
 	return ebt, eat, ratio
+}
+
+func writeResultsToFile(ebt, eat, ratio float64) {
+	earnings := fmt.Sprintf("Earnings Before Tax:%.2f\nEarnings After Tax:%.2f\nRatio:%.2f", ebt, eat, ratio)
+	os.WriteFile("results.txt", []byte(earnings), 0644)
 }
