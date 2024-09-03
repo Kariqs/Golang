529ebbb11a3622ffce4564ac4fb1e783cb36ad48
Kariqs <kariukibenard189@gmail.com>
2024-09-03 12:51:23 +0300

    Learned about generics

--- generics/sum.go
+++ generics/sum.go
@@ -10,3 +10,5 @@ func main() {
 func add[T int | float64](a, b T) T {
 	return a + b
 }
+
+//In the above function we want the user to input any value and the function should return the type of the input value
